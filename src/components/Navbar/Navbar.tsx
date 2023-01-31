c1bf795a07401adc79e53dcdaa6bce3d1b29654e
Godswill Ezihe <ezihegodswill01@gmail.com>
2023-01-31 06:20:01 +0100

    chore: add navbar styles and markupo

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,56 @@
+import { Link, NavLink } from "react-router-dom";
+import {
+  FaSearch,
+  FaWallet,
+  FaRegUserCircle,
+  FaShoppingCart,
+} from "react-icons/fa";
+import "./Navbar.css";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="logo">
+          NFTEarth
+        </Link>
+        <div className="input-box">
+          <FaSearch className="search-icon" />
+          <input
+            type="search"
+            name="s"
+            id="s"
+            placeholder="Search for a collection"
+          />
+        </div>
+        <nav className="nav">
+          <ul className="nav-list">
+            <li className="nav-item">
+              <NavLink to="/">Stats</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/">Sell</NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/">
+                <FaRegUserCircle size={20} />
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/">
+                <FaShoppingCart size={20} />
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/">
+                <FaWallet size={20} />
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
