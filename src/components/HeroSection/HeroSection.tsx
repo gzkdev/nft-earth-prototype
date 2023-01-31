5998dc309cf683849390de57c5726ef9c4d86930
Godswill Ezihe <ezihegodswill01@gmail.com>
2023-01-31 06:33:05 +0100

    chore: ui update

---
+++ src/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,13 @@
+import "./HeroSection.css";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <div className="container">
+        <h1>Buy and Sell NFTs on Layer 2 Blockchain Solutions</h1>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import AppLayoutContainer from "../components/AppLayoutContainer";
+import HeroSection from "../components/HeroSection";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <AppLayoutContainer>
       <Navbar />
+      <HeroSection />
     </AppLayoutContainer>
   );
 };
